4db2923bf17157f3a082e8b330f5e45a67a2f998
fairy26 <yoshida-yosei-vx@ynu.jp>
2023-01-17 19:37:52 +0900

    add: 型ヒントを追加

--- notion_toys/logger/logger.py
+++ notion_toys/logger/logger.py
@@ -1,5 +1,5 @@
 from importlib import resources
-from logging import getLogger
+from logging import Logger, getLogger
 from logging.config import dictConfig
 
 import yaml
@@ -8,11 +8,11 @@ CONFIGFILE = "log_config.yaml"
 LOGGERNAME = "notion_toys"
 
 
-def init_logger():
+def init_logger() -> None:
     with resources.path("logger", CONFIGFILE) as log_config:
         with open(log_config, encoding="utf-8") as f:
             dictConfig(yaml.safe_load(f))
 
 
-def get_logger():
+def get_logger() -> Logger:
     return getLogger(LOGGERNAME)
