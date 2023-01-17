0631f128bbddfe3c047034a5ab7a60b5dd898282
fairy26 <yoshida-yosei-vx@ynu.jp>
2023-01-17 19:42:29 +0900

    add: filmarksモードを追加

--- notion_toys/argparser.py
+++ notion_toys/argparser.py
@@ -5,5 +5,6 @@ def parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-q", "--quiet", default=False, action=argparse.BooleanOptionalAction, help="quiet mode")
+    parser.add_argument("-f", "--filmarks", action="store_true")
 
     return parser.parse_args()
